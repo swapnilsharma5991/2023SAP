892d77ff1c77ad33838995f90da411649c060a9e
swapnil <swapnilcv2016@gmail.com>
2023-10-12 09:54:24 +0530

    Added : Rediff_Mobile

---
+++ src/test/java/tests/Rediff_Mobile.java
@@ -0,0 +1,40 @@
+package tests;
+
+import org.testng.annotations.Test;
+import java.io.IOException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import com.Rediff.Constants.Constants;
+import com.qa.Rediff.base.base;
+
+public class Rediff_Mobile extends base {
+	
+	public Rediff_Mobile() throws IOException {
+		super();
+		
+	}
+
+
+	@BeforeTest
+	public void setUp() throws IOException{
+		initializeDriver();	
+		System.out.println("Before");	
+	}
+	
+	@Test(priority=1)
+	public void rediffmethod1() throws IOException, InterruptedException {
+		System.out.println("Test 1 shopping");	
+	}
+	
+	@AfterTest
+	public void tearDown() throws InterruptedException {
+		System.out.println("Teardown");
+		Thread.sleep(1000);
+		driver.quit();
+	}
+}
